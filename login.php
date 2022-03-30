33323b1a6d19e5c817d8af578eadcb73b080d390
Rahul <mtal3584@gmail.com>
2022-03-30 18:32:57 +0545

    added

---
+++ login.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <form method=post action="db/login.php">
+            <label>Email Address</label>
+            <input type="text" name="email" placeholder="Enter email adress"></input>
+            <label>Password </label>
+            <input type="text" name="password" placeholder="Enter Password"></input>
+
+            <input type="submit" value="Login"></input>
+        </form>
+
+            <h1>Not signed up yet</h1>
+            <a href="signup.php"><button>Sign Up</button></a>
+
+            <h1>Login as employee</h1>
+            <a href=employee_login.php><button>Employee Login</button></a>
+
+    </body>
+</html>
